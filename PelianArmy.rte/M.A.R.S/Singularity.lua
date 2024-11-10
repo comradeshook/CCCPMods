eef2598127b7546a4da6adfdd519699f8228202f
comradeshook <comradeshook@gmail.com>
2024-11-10 13:16:50 +0100

    Optimised singularity script and made it only gib things that actually have gibs

--- PelianArmy.rte/M.A.R.S/Singularity.lua
+++ PelianArmy.rte/M.A.R.S/Singularity.lua
@@ -5,6 +5,7 @@ function Create(self)
 	var.killRadius = 15
 	var.ownerVector = Vector(-50, 0):RadRotate(self.RotAngle)
 	var.owner = MovableMan:GetMOFromID(SceneMan:CastMORay(self.Pos, var.ownerVector, self.ID, -2, 0, false, 1))
+	var.Pos = self.Pos;
 	if var.owner ~= nil then
 		var.ownerRoot = MovableMan:GetMOFromID(var.owner.RootID)
 		if var.ownerRoot:HasObject("MARS Singularity Cannon") == false then
@@ -20,20 +21,30 @@ function Create(self)
 	self.var = var;
 end
 
+local ShortestDistance = SceneMan.ShortestDistance;
+
 function Update(self)
 	local var = self.var;
-	for MO in MovableMan:GetMOsInRadius(self.Pos, var.pullRadius, -1, false) do
-		if MO:GetRootParent().UniqueID ~= var.ownerRoot.UniqueID then
-			local MOVector = SceneMan:ShortestDistance(self.Pos, MO.Pos, true)
-			self.gravDist = MOVector.Magnitude
+	local GetRootParent = self.GetRootParent;
+	local RadRotate = var.Pos.RadRotate;
+	for MO in MovableMan:GetMOsInRadius(var.Pos, var.pullRadius, -1, false) do
+		if GetRootParent(MO).UniqueID ~= var.ownerRoot.UniqueID then
+			local MOVector = ShortestDistance(SceneMan, var.Pos, MO.Pos, true)
+			local gravDist = MOVector.Magnitude
 			if MOVector.Magnitude < var.killRadius + (MO.Radius / 2) then
-				if MO.ClassName ~= "MOPixel" and MO.ClassName ~= "MOSParticle" then
-					ToMOSRotating(MO):GibThis()
+				if IsMOSRotating(MO) then
+					local hasGibs = false;
+					for gib in ToMOSRotating(MO).Gibs do
+						hasGibs = true;
+					end
+					if (hasGibs) then
+						ToMOSRotating(MO):GibThis()
+					end
 				end
 			end
 			if MO.PresetName ~= "PA MARS" then
-				local angle = math.atan2(-MOVector.Y, MOVector.X)
-				local pullStrength = Vector((-var.maxPull * (5 / self.gravDist)), 0):RadRotate(angle)
+				local angle = MOVector.AbsRadAngle;
+				local pullStrength = RadRotate(Vector((-var.maxPull * (5 / gravDist)), 0), angle)
 				MO.Vel = MO.Vel + pullStrength
 			end
 		end
